98dab41558b846fe933587fcc1cbf197c09877d2
ahmad luqman hakim <ahmad luqman hakim@MSI>
2024-09-18 15:14:12 +0700

    first comitT 1

---
+++ src/main/java/com/mycompany/kelompok_enam/Kelompok_enam.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kelompok_enam;
+
+/**
+ *
+ * @author ahmad luqman hakim
+ */
+public class Kelompok_enam {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
